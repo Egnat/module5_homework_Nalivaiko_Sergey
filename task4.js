7038c90c3cd77ebb43211d9ce733ae94adf4cea5
Egnat <sergey_p_i@mail.ru>
2022-08-06 22:55:49 +0300

    (Module 5 JS)

--- task4.js
+++ task4.js
@@ -1,4 +1,7 @@
-let getRandomIntInclusive = (max) => {
-return Math.floor(Math.random() * max);
+let numArr = [0, 100]
+function getRandomIntInclusive(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
-console.log(getRandomIntInclusive(101));
+console.log(getRandomIntInclusive(0, 100))
