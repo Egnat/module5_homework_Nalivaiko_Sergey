ac0b80b4dc301cb8a37e5f35f5ab21b576023391
Egnat <sergey_p_i@mail.ru>
2022-08-06 22:02:25 +0300

    (Modul 5 JS)

--- task6.js
+++ task6.js
@@ -1,6 +1,6 @@
 let numArr = [3, 55, 8, 7, 77, 3, 20];
 let check = numArr[0];
-numArr.forEach((item, index, numArr) => {
+numArr.forEach(function(item, index, numArr) {
 if(item == check) {
 console.log(`true`);
 } else  {
