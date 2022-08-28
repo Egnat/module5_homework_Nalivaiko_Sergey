a5c0ffac27446538a8ef62711532b9cf5ab2b4a1
Egnat <sergey_p_i@mail.ru>
2022-08-29 00:09:11 +0300

    (js)

--- task5.js
+++ task5.js
@@ -2,14 +2,22 @@
 //затем перебрать его и вывести в консоль каждый элемент массива.
 
 let numArr = [1, 2, 3, 4, 5, 6, 7];
-console.log(numArr);
-console.log(numArr.length);
+  console.log(numArr);
+  console.log(numArr.length);
 for (let i = 0; i <numArr.length; i += 1) {
-console.log(numArr[i]);
+  console.log(numArr[i]);
 }
-let sum = 0;
+let sum = 0; //эио просто так, это я сделал дополнительно
 for (let i = 0; i <numArr.length; i ++) {
-sum += numArr[i];
-debugger
+  sum += numArr[i];
+  debugger
 }
-console.log(sum);
+console.log(sum); // вывел сумму в консоль
+
+/////////////////////
+
+let arr = [1, 3, 5];
+  console.log(arr);
+arr.map(function(arr) {
+  console.log(arr);
+});
--- task8.js
+++ task8.js
@@ -3,14 +3,16 @@
 
 Используйте шаблонные строки.*/
 
-let people = new Map()
+let people = new Map();
 people.set(`${"x"} - Катя`, `${"y"} - 18`);
 people.set(`${"x"} - Федя`, `${"y"} - 22`);
 people.set(`${"x"} - Вася`, `${"y"} - 26`);
-
-console.log(people.keys()) 
-console.log(people.values()) 
-
 for ( let name of people.keys()) {
-console.log(`ключи и свойства, ключ - ${name}`, people)
+  console.log(`ключи и свойства, ключ - ${name}`, people);
 }
+for ( let age of people.values()) {
+  console.log(`ключи и свойства, значения - ${age}`, people);
+}
+
+console.log(people.keys());
+console.log(people.values()); 
